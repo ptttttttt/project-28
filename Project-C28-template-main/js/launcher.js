d41440c89426abfe1527d31697a500fcd24fa756
ptttttttt <74365558+ptttttttt@users.noreply.github.com>
2021-06-25 18:00:35 +0530

    Add files via upload

---
+++ Project-C28-template-main/js/launcher.js
@@ -0,0 +1,31 @@
+class launcher{
+    constructor(body,anchor)
+	{
+		var options={
+            bodyA:body,
+            pointB:anchor,
+            stiffness:0.003,
+            length:2
+			}
+		this.bodyA=body;
+		this.pointB=anchor;
+		this.launcher=Constraint.create(options)
+		
+		World.add(world, this.launcher);
+	}
+    attach(body){
+        this.launcher.bodyA=body;
+    }
+    fly(){
+        this.launcher.bodyA=null;
+    }
+	display()
+	{
+		if(this.launcher.bodyA){
+            var pointA=this.bodyA.position;
+            var pointB=this.pointB;
+            strokeWeight(3);
+            line(pointA.x,pointA.y,pointB.x,pointB.y);
+        } 
+ }
+}
